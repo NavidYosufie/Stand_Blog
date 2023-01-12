1952dc278dff7dfaab75128b562ded687f24e435
navid <navid.yosufie4@gmail.com>
2023-01-12 07:43:20 -0800

    class base view

--- blog/urls.py
+++ blog/urls.py
@@ -5,9 +5,10 @@ app_name = "blog"
 
 urlpatterns = [
     path("all-article", views.article_list, name="blog"),
-    path("detail/<slug:slug>", views.post_detail, name="article_detail"),
+    path("about/", views.about, name="about"),
+    path("detail/<slug:slug>", views.ArticleDetailView.as_view(), name="article_detail"),
     path("cateqory_detail/<slug:slug>", views.cateqory_detail, name="category_detail"),
     path("search/", views.search_article, name="search_articles"),
     path("contactus/", views.contactus, name="contact_us"),
-    path("art/", views.ArticleList.as_view(), name="contact_us")
+    path("art/", views.ArticleList.as_view(), name="Aer"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,16 +4,19 @@ from django.core.paginator import Paginator
 from .forms import ContactUsForm, MessageForm
 from django.views import View
 from django.contrib.auth.models import User
-from django.views.generic import ListView
-from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.base import TemplateView, RedirectView
 
-def post_detail(request, slug):
+def article_detail(request, slug):
     article = get_object_or_404(Article, slug=slug)
     if request.method == 'POST':
         body = request.POST.get("body")
         parent_id = request.POST.get("parent_id")
         Comment.objects.create(body=body, article=article, user=request.user, parent_id=parent_id)
     return render(request, "blog/article_detail.html", {"article": article})
+
+def about(request):
+    return render(request, "blog/about.html")
 
 
 def article_list(request):
@@ -56,10 +59,21 @@ def contactus(request):
         form = MessageForm()
     return render(request, "blog/contact_us.html", {"form": form})
 
-class ArticleList(TemplateView):
-    template_name = "blog/article_list.html"
 
+
+
+class ArticleList(TemplateView):
+    template_name = "blog/user_list.html"
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["object_list"] = Article.objects.all()[:4]
-        
+        context['object_list'] = User.objects.all()
+        return context
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    
+
+
+
+
